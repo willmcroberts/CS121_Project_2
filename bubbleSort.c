0c1057bc192e5378a42dc04dae6c40413a525437
willmcroberts <williammcroberts15@gmail.com>
2025-01-22 18:04:48 +0000

    Added starter code

---
+++ bubbleSort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+const int MAX=9;
+
+void printValues(int*);
+void sort(int*);
+void swap(int*, int*);
+
+int main(){
+  int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+  printf("Before: \n");
+  printValues(values);
+
+  // test swap
+  int x = 3;
+  int y = 5;
+  printf("x: %d, y: %d \n", x, y);
+  swap(&x, &y);
+  printf("x: %d, y: %d \n", x, y);
+
+  sort(values);
+  printf("After: \n");
+  printValues(values);
+
+  return(0);
+} // end main
